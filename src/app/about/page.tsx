03a83c3dd7c7f4b38a88218d80258757842af7e5
Carlton Joseph <carlton.joseph@gmail.com>
2023-11-01 15:38:00 -0700

    add about

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default async function About() {
+  return (
+    <main className="flex flex-col max-w-lg mx-auto items-center gap-2 mt-3">
+      <h2 className="text-xl font-semibold">Site Version 0.0.1</h2>
+    </main>
+  );
+}
